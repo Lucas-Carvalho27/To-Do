2e1fe33d5f7de064fec128b220247f610f3a92b6
Lucas-Carvalho27 <lucascastroeeeh@gmail.com>
2025-05-09 23:44:43 -0300

    Code to initialize DB

---
+++ init.sql
@@ -0,0 +1,43 @@
+-- initializing tables
+
+CREATE TABLE User (
+    UserID INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Day (
+    DayID INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE TaskList (
+    TaskListID INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    parentId INT DEFAULT NULL,
+    complete BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (parentId) REFERENCES TaskList(TaskListID) ON DELETE CASCADE
+);
+
+CREATE TABLE UserDayLists (
+    UserID INT,
+    DayID INT,
+    TaskListID INT,
+    PRIMARY KEY (UserID, DayID, TaskListID),
+    FOREIGN KEY (UserID) REFERENCES User(UserID),
+    FOREIGN KEY (DayID) REFERENCES Day(DayID),
+    FOREIGN KEY (TaskListID) REFERENCES TaskList(TaskListID)
+);
+
+CREATE TABLE Tasks (
+    TaskID INT PRIMARY KEY AUTO_INCREMENT,
+    TaskListID INT,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    complete BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (TaskListID) REFERENCES TaskList(TaskListID) ON DELETE CASCADE
+);
+
+
+INSERT INTO Day (name) VALUES 
+('Monday'), ('Tuesday'), ('Wednesday'), 
+('Thursday'), ('Friday'), ('Saturday'), ('Sunday');
